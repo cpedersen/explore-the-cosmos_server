6ed4876dfa5440f43e6b8a940d5c55581dc7f3c8
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-23 18:49:25 -0500

    Add console logs

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,12 +1,39 @@
 require("dotenv").config();
+const url = require("url");
+console.log("url ", url);
+
+let host;
+let port;
+let database;
+let username;
+let password;
+if (process.env.DATABASE_URL) {
+  const dbUrl = url.parse(process.env.DATABASE_URL);
+  console.log("dbUrl ", dbUrl);
+  host = dbUrl.hostname;
+  console.log("host ", host);
+  port = dbUrl.port;
+  console.log("port ", port);
+  database = dbUrl.pathname.replace(/^\//, "");
+  console.log("database ", database);
+  [(username, password)] = dbUrl.auth.split(":");
+  console.log("username ", username);
+  console.log("password ", password);
+}
+console.log("host ", host);
+console.log("port ", port);
+console.log("database ", database);
+console.log("username ", username);
+console.log("password ", password);
 
 module.exports = {
-  migrationsDirectory: "migrations",
+  migrationDirectory: "migrations",
   driver: "pg",
-  connectionString:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+  host,
+  port,
+  database,
+  username,
+  password,
   ssl: {
     rejectUnauthorized: false,
   },
