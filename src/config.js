250382731b4a5a06d29204a3424db7940c2cd3a6
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-24 22:37:50 -0500

    Adding password

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ module.exports = {
   CLIENT_ORIGIN: "http://localhost:3000",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://dunder_mifflin@localhost/cosmos_quotes",
+    "postgresql://dunder_mifflin:password@localhost/cosmos_quotes",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
+    "postgresql://dunder_mifflin:password@localhost/cosmos_quotes_test",
   GOOGLE_APPLICATION_CREDENTIALS:
     process.env.GOOGLE_APPLICATION_CREDENTIALS || "http://localhost:8000/api",
 };
