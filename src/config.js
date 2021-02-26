7973109f8341a91b8b29083f1b7463c59f26c0ae
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-26 01:15:24 -0500

    Change db env var

--- src/config.js
+++ src/config.js
@@ -24,9 +24,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "production",
   CLIENT_ORIGIN:
     CLIENT_ORIGIN || "https://explore-the-cosmos-client.vercel.app",
-  DATABASE_URL:
-    DATABASE_URL ||
-    "postgres://wjjjcrerskmfmg:f8111be0f0a861c25d4f2e010375eabc641056441dbb96be58467608d5d8ff2c@ec2-35-171-57-132.compute-1.amazonaws.com:5432/dte2fdjjs0vdm",
+  DATABASE_URL: DATABASE_URL || process.env.DATABASE_URL,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
