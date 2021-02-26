83943fee9b2679b936bf9678dc54d26c1544240e
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-25 22:35:04 -0500

    Added console log

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
+const CLIENT_ORIGIN =
+  process.env.NODE_ENV === "production"
+    ? process.env.CLIENT_ORIGIN_PROD
+    : process.env.CLIENT_ORIGIN_LOCAL;
+
+console.log("CLIENT_ORIGIN: ", CLIENT_ORIGIN);
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "http://localhost:3000",
+  CLIENT_ORIGIN: CLIENT_ORIGIN,
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/cosmos_quotes",
