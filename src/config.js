e43e873e06d09213c2602542d08a9e4addd1daa4
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-25 23:25:04 -0500

    Update variables for remote access

--- src/config.js
+++ src/config.js
@@ -19,13 +19,16 @@ console.log("CLIENT_ORIGIN: ", CLIENT_ORIGIN);
 console.log("DATABASE_URL: ", DATABASE_URL);
 
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: CLIENT_ORIGIN,
-  DATABASE_URL: DATABASE_URL,
+  PORT: PORT || 5432,
+  NODE_ENV: process.env.NODE_ENV || "production",
+  CLIENT_ORIGIN:
+    CLIENT_ORIGIN || "https://explore-the-cosmos-client.vercel.app",
+  DATABASE_URL:
+    DATABASE_URL || "postgresql://dunder_mifflin@localhost/cosmos_quotes",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
   GOOGLE_APPLICATION_CREDENTIALS:
     process.env.GOOGLE_APPLICATION_CREDENTIALS || "http://localhost:8000/api",
+  MIN_VISION_TAG_SCORE: process.env.MIN_VISION_TAG_SCORE || "0.1",
 };
