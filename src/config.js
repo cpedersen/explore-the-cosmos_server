cc989acda7a0dd75d9a7089494890534b786be1c
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-25 22:51:31 -0500

    Add better support for DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -5,15 +5,19 @@ const CLIENT_ORIGIN =
     ? process.env.CLIENT_ORIGIN_PROD
     : process.env.CLIENT_ORIGIN_LOCAL;
 
+const DATABASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL_PROD
+    : process.env.DATABASE_URL_LOCAL;
+
 console.log("CLIENT_ORIGIN: ", CLIENT_ORIGIN);
+console.log("DATABASE_URL: ", DATABASE_URL);
 
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: CLIENT_ORIGIN,
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgresql://dunder_mifflin@localhost/cosmos_quotes",
+  DATABASE_URL: DATABASE_URL,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
