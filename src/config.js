8e2616f9f17b8e52c7915ed169f1af7bbc06c751
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-26 01:10:58 -0500

    Tweak google ref in config

--- src/config.js
+++ src/config.js
@@ -24,11 +24,12 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "production",
   CLIENT_ORIGIN:
     CLIENT_ORIGIN || "https://explore-the-cosmos-client.vercel.app",
-  DATABASE_URL: DATABASE_URL || process.env.DATABASE_URL,
+  DATABASE_URL:
+    DATABASE_URL ||
+    "postgres://wjjjcrerskmfmg:f8111be0f0a861c25d4f2e010375eabc641056441dbb96be58467608d5d8ff2c@ec2-35-171-57-132.compute-1.amazonaws.com:5432/dte2fdjjs0vdm",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
-  GOOGLE_APPLICATION_CREDENTIALS:
-    process.env.GOOGLE_APPLICATION_CREDENTIALS || "http://localhost:8000/api",
+  GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   MIN_VISION_TAG_SCORE: process.env.MIN_VISION_TAG_SCORE || "0.1",
 };
