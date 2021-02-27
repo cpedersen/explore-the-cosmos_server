060081d296f5e4887322517c7dbcce59b8b0a989
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-02-26 23:35:05 -0500

    Debug

--- src/config.js
+++ src/config.js
@@ -19,12 +19,9 @@ console.log("PORT: ", PORT);
 module.exports = {
   PORT,
   NODE_ENV: process.env.NODE_ENV || "production",
-  CLIENT_ORIGIN:
-    CLIENT_ORIGIN || "https://explore-the-cosmos-client.vercel.app",
-  DATABASE_URL: DATABASE_URL || process.env.DATABASE_URL,
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    "postgresql://dunder_mifflin@localhost/cosmos_quotes_test",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || CLIENT_ORIGIN,
+  DATABASE_URL: process.env.DATABASE_URL || DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   MIN_VISION_TAG_SCORE: process.env.MIN_VISION_TAG_SCORE || "0.1",
 };
