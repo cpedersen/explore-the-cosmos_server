1dcfafb793f844db224f39cd22d333d2c9e136d1
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-01 21:40:06 -0500

    Debug

--- src/config.js
+++ src/config.js
@@ -12,6 +12,9 @@ const DATABASE_URL =
     : process.env.DATABASE_URL_LOCAL;
 //? `${process.env.DATABASE_URL}?sslmode=require`
 
+const DATABASE_URL =
+  "postgres://wjjjcrerskmfmg:f8111be0f0a861c25d4f2e010375eabc641056441dbb96be58467608d5d8ff2c@ec2-35-171-57-132.compute-1.amazonaws.com:5432/dte2fdjjs0vdm";
+
 let GOOGLE_APPLICATION_CREDENTIALS =
   process.env.NODE_ENV === "production"
     ? process.env.GOOGLE_APPLICATION_CREDENTIALS
