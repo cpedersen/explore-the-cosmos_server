092b90ca55ddd2e5561538f0c193a01cb645e400
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-01 19:36:54 -0500

    Debug

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
+  //connection: DATABASE_URL,
+  connection: `${DATABASE_URL}?ssl=true`,
 });
 
 app.set("db", db);
 
 app.listen(PORT, () => {
-  console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
   console.log(`Server listening at http://localhost:${PORT}`);
 });
