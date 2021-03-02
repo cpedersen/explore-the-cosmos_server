daa46490022fda28ceead773c129f94901738a08
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-01 23:19:59 -0500

    Debug

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const db = knex({
   client: "pg",
   connection: DATABASE_URL,
   //connection: `${DATABASE_URL}?sslmode=require`,
-  //ssl: { require: true, rejectUnauthorized: false },
+  ssl: { require: true, rejectUnauthorized: false },
   //ssmode: require,
 });
 
