d213b059e612f14e52a6820edb1b85dedac7aacb
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-02 19:40:27 -0500

    Debug

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
+  //connection: DATABASE_URL,
   //connection: `${DATABASE_URL}?sslmode=require`,
+  connection: `${DATABASE_URL}?pgsslmode=no-verify`,
   //ssl: { require: true, rejectUnauthorized: false },
   //pgsslmode: no-verify,
   //sslmode: require,
