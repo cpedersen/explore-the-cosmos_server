b388aa7565645f3d4ff89b6eab162e9fba3ad95b
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-02 20:02:55 -0500

    Debug

--- src/server.js
+++ src/server.js
@@ -5,8 +5,11 @@ const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require("./config");
 const db = knex({
   client: "pg",
   connection: DATABASE_URL,
-  // On Heroku, set the following config var:
+  // On Heroku, set the following config var (https://help.heroku.com/966620):
   // PGSSLMODE: no-verify
+
+  // None of the following worked when set in this file,
+  // but I'm keeping this here for reference:
   //connection: `${DATABASE_URL}?sslmode=require`,
   //connection: `${DATABASE_URL}?pgsslmode=no-verify`,
   //ssl: { require: true, rejectUnauthorized: false },
